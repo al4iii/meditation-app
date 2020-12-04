ec4476878adb601db163477d8524d053afb94c4a
Aliaksandr Danilin <al4icrux@gmail.com>
2020-12-04 12:28:19 +0300

    fix: changed addEventListener;

--- main.js
+++ main.js
@@ -3,18 +3,17 @@ const play = document.querySelector(".play");
 const replay = document.querySelector(".replay");
 const outline = document.querySelector(".moving-outline circle");
 const video = document.querySelector(".vid-container video");
-//Sounds
 const sounds = document.querySelectorAll(".sound-picker button");
-//Time Display
 const timeDisplay = document.querySelector(".time-display");
 const outlineLength = outline.getTotalLength();
-//Duration
 const timeSelect = document.querySelectorAll(".time-select button");
 let fakeDuration = 600;
-
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
 timeDisplay.textContent = `${Math.floor(fakeDuration / 60)} : ${Math.floor(fakeDuration % 60)}`;
+
+play.addEventListener("click", () => checkPlaying(song));
+replay.addEventListener("click", () => restartSong(song));
 
 sounds.forEach(sound => {
   sound.addEventListener("click", function() {
@@ -22,18 +21,10 @@ sounds.forEach(sound => {
     video.src = this.getAttribute("data-video");
     checkPlaying(song);
   });
-});
-
-play.addEventListener("click", function() {
-  checkPlaying(song);
-});
-
-replay.addEventListener("click", function() {
-    restartSong(song);    
-  });
-
+});    
+ 
 const restartSong = song => {
-    let currentTime = song.currentTime;
+    let currentTime = song.currentTime;    
     song.currentTime = 0;    
 };
 
